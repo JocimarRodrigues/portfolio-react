6e1f52630968b709c08a2d3c7495eb5a2d871bf8
jocimar <jocimarrodrigues1337@gmail.com>
2023-03-16 13:04:22 -0300

    Pagina Inicial Finalizada

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CabecalhoLink from '../CabecalhoLink';
+import CabecalhoLink from 'components/CabecalhoLink';
 import styles from "./Cabecalho.module.scss";
 
 
--- src/pages/PaginaInicial/index.jsx
+++ src/pages/PaginaInicial/index.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import foto from "./megumin.jpg";
 import styles from "./PaginaInicial.module.scss";
 
+
+
 function PaginaInicial() {
+  const nomeRef = useRef(null);
+
+  useEffect(() => {
+    if (nomeRef.current) {
+      animacaoNoNome(nomeRef.current);
+    }
+  }, []);
+
+  function animacaoNoNome(e) {
+    const textArray = e.innerHTML.split('');
+    e.innerHTML = '';
+    textArray.forEach((letra, i) => {
+      setTimeout(function() {
+        e.innerHTML += letra
+      }, 75 * i)
+    })
+  }
+
+  let mostrarConteudo = () => {
+    let conteudo = document.querySelector('#conteudo')
+    conteudo.style.opacity = 1;
+    conteudo.style.transition = '1.8s'
+  }
+
+
+
+
+
   return (
     <main>
       <section>
         <div className={styles.apresentacao}>
           <img src={foto} alt="" />
-          <h1>Olá, me chamo</h1>
-          <span>Jocimar Rodrigues</span>
-          <h2>Sou Desenvolvedor Front-End</h2>
+          <h1 className={styles.texto}>Olá, me chamo</h1>
+          <span ref={nomeRef} className={styles.texto}>Jocimar Rodrigues</span>
+          <h2 className={styles.texto}>Sou Desenvolvedor Front-End</h2>
             <ul>
               <li>
               <a  href="https://www.linkedin.com/in/jocimar-rodrigues-chagas/">
@@ -28,16 +58,22 @@ function PaginaInicial() {
               </a>
               </li>
             </ul>
-          
-
         </div>
       </section>
-      <section className={styles.sobre}>
-        <div>
-
+      <section className={styles.sobre} id="conteudo" onWheel={mostrarConteudo} onScroll={mostrarConteudo} onMouseEnter={mostrarConteudo}>
+        <div className={styles.conteudo}>
+          <h1>Sobre Mim</h1>
+          <p>Olá, meu nome é Jocimar Rodrigues, sempre fui apaixonado por tecnologia, mas nunca tinha tido contato
+                    com a programação, até pouco tempo, quando um amigo ingressou no mercado e me explicou como o mesmo
+                    funciona e a prática. Decidi me dedicar e tentar aprender e, quando o fiz, me identifiquei com a
+                    área. Programar acabou virando um hobby, fiquei fascinado em aprender novas linguagens e resolver
+                    problemas. Atualmente estou estudando com
+                    foco em me tornar dev Front-End, neste portfólio você poderá ver os projetos que desenvolvi até
+                    agora e poderá acompanhar minha evolução.</p>
         </div>
       </section>
     </main>
+
   );
 }
 
