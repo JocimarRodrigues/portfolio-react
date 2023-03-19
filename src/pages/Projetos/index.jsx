4f412a957dc1e846b6e2036af91a9a0b487d7575
jocimar <jocimarrodrigues1337@gmail.com>
2023-03-19 07:52:01 -0300

    Estilização da página de Projetos Finalizada

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,37 +1,40 @@
-import Carousel from 'components/Carousel';
-import React from 'react'
-import styles from './Projetos.module.scss';
+import Carousel from "components/Carousel";
+import React from "react";
+import styles from "./Projetos.module.scss";
 
 function Projetos() {
   return (
-    <body className={styles.projetos}>
-      
-    <main>
-
-    <section>
-
-    <div className={styles.container}>Projetos
-    <h1>Projetos em Javascript</h1>
-    <Carousel>
-      <li>
-        <img src="/imagens/fujiwara.png" alt="" />
-      </li>
-    </Carousel>
-    </div>
+    <section className={styles.container}>
+      Projetos
+      <h1>Projetos em Javascript</h1>
+      <Carousel>
+        <div className={styles.lista}>
+          <ul>
+            <li>
+              <img src="/imagens/fujiwara.png" alt="" />
+            </li>
+            <li>
+              <img src="/imagens/ania.jpg" alt="" />
+            </li>
+            <li>
+              <img src="/imagens/megumin2.png" alt="" />
+            </li>
+          </ul>
+        </div>
+      </Carousel>
+      <section>
+        <div>
+          <h1>Projetos em React</h1>
+          <Carousel>
+            <li>
+              <img src="/imagens/megumin2.png" alt="" />
+              <img src="/imagens/ania.jpg" alt="" />
+            </li>
+          </Carousel>
+        </div>
+      </section>
     </section>
-    <section>
-      <div>
-        <h1>Projetos em React</h1>
-        <Carousel>
-          <li>
-            <img src="" alt="" />
-          </li>
-        </Carousel>
-      </div>
-    </section>
-    </main>
-    </body>
-  )
+  );
 }
 
-export default Projetos
+export default Projetos;
