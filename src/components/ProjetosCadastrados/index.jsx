5a0ab3c23aa53edf40bfcf7e5c248373ba360b7e
jocimar <jocimarrodrigues1337@gmail.com>
2023-03-22 08:29:27 -0300

    Atualizando

---
+++ src/components/ProjetosCadastrados/index.jsx
@@ -0,0 +1,64 @@
+import Modal from "components/Modal";
+import React, { useState } from "react";
+
+
+
+import styles from "./ProjetosCadastrados.module.scss"
+
+function ProjetosCadastrados({ id, imagem, descricao, tags, linkGitHub, linkDeploy }) {
+
+  const [mostrarModal, setMostrarModal] = useState(false);
+
+
+  const abrirModal = () => {
+    setMostrarModal(true);
+  }
+
+  const fecharModal = () => {
+    setMostrarModal(false);
+  }
+
+
+  return (
+    <>
+    <div className={styles.container}>
+
+        <li>
+          <img src={imagem} alt={descricao} onClick={abrirModal}/>
+        </li>
+
+    </div>
+    <div>
+      <Modal 
+      mostrarModal={mostrarModal}
+      fecharModal={fecharModal}
+      id={id}
+      descricao={descricao}
+      imagem={imagem}
+      tags={tags}
+      linkGitHub={linkGitHub}
+      linkDeploy={linkDeploy}
+      />
+    </div>
+    </>
+
+  );
+}
+
+export default ProjetosCadastrados;
+
+
+/* <ul>
+{projetos.map((item) => {
+  return (
+    <li key={item.id}>
+      <img src={item.imagem} 
+      onClick={abrirModal}/>
+      <Modal descricao={item.descricao}
+
+      mostrarModal={mostrarModal}
+      fecharModal={fecharModal}
+      />
+    </li>
+  )
+})} */
